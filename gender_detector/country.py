9d0d3201eb46ab04f6745d0d7b91698d8c2739a7
malev <marcosvanetta@gmail.com>
2014-05-09 12:51:28 -0500

    Make sure data file exists

--- gender_detector/country.py
+++ gender_detector/country.py
@@ -10,6 +10,8 @@ class Country:
     '''
     def __init__(self, country='us'):
         self.country = country
+        if not os.path.isfile(self.file()):
+            raise RuntimeError("There is no data file for that coutry")
 
     def file(self):
         return self.base_directory() + 'data/%sprocessed.csv' % (self.country)
