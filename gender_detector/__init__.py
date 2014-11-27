226f5b19f7e8c56f06bf57ba8cd6a203d44c07e9
Marcos Vanetta <marcosvanetta@gmail.com>
2014-11-27 17:00:13 +0300

    Bump to version 0.0.6

--- gender_detector/__init__.py
+++ gender_detector/__init__.py
@@ -2,5 +2,5 @@ from gender_detector import *
 
 __copyright__ = 'Copyright (c) 2014 Marcos Vanetta'
 
-version = (0,0,5)
+version = (0,0,6)
 version_string = "Gender Detector version %d.%d.%d" % version
