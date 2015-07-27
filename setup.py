176218d2b05b1d05d0b82e26cc1a11a8aceff32d
malev <marcosvanetta@gmail.com>
2015-07-26 23:33:25 -0500

    Styling fixes

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ from gender_detector import version
 version = '.'.join([str(x) for x in version])
 
 setup(name='gender-detector',
-    version = version,
-    description = "Gender detection library",
-    author = "Marcos Vanetta",
-    author_email = "marcosvanetta@gmail.com",
-    url = "http://codingnews.org",
-    license = 'GPL2',
-    keywords = "gender detection",
-    packages = ['gender_detector', 'gender_detector.tests'],
-    package_data = {
+    version=version,
+    description="Gender detection library",
+    author="Marcos Vanetta",
+    author_email="marcosvanetta@gmail.com",
+    url="http://codingnews.org",
+    license='GPL2',
+    keywords="gender detection",
+    packages=['gender_detector', 'gender_detector.tests'],
+    package_data={
         'gender_detector': ['data/*.csv']
     }
 )
